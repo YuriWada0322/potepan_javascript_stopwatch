29a3bb2ed6dbfeb523a1f6d96ee4f1de66455af5
EC2 Default User <ec2-user@ip-172-31-1-191.us-east-2.compute.internal>
2021-05-07 11:40:39 +0000

    javascript stopwatch second commit

--- main.js
+++ main.js
@@ -1,43 +1,49 @@
-const timer=document.getElementById("timer");
-const start=document.getElementById("start");
-const stop=document.getElementById("stop");
-const reset=document.getElementById("reset");
+const timer = document.getElementById("timer");
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
 
-let elapsed=0;
+let elapsed = 0;
 
-let intervalId=null;
+let intervalId = null;
 
 function updateTime(){
-   const ms=elapsed%1000;
-   const s=Math.floor(elapsed/1000)%60;
-   const m=Math.floor(elapsed/(1000*60))%60;
-   const h=Math.floor(elapsed/(1000*60*60));
+   const ms = elapsed%1000;
+   const s = Math.floor(elapsed / 1000) % 60;
+   const m = Math.floor(elapsed / (1000 * 60)) % 60;
+   const h = Math.floor(elapsed / (1000 * 60 * 60));
    
-   const msStr=ms.toString().padStart(1,`0`);
-   const sStr=s.toString().padStart(1,`0`);
-   const mStr=m.toString().padStart(1,`0`);
-   const hStr=h.toString().padStart(1,`0`);
+   const msStr = ms.toString().padStart(1,`0`);
+   const sStr = s.toString().padStart(1,`0`);
+   const mStr = m.toString().padStart(1,`0`);
+   const hStr = h.toString().padStart(1,`0`);
    
-   timer.innerHTML=`${hStr}:${mStr}:${sStr}:${msStr}`;
+   timer.innerHTML = `${hStr}:${mStr}:${sStr}:${msStr}`;
 }
    
 start.addEventListener('click',function(e){
+   $("#start").prop('disabled', true);
+   $("#stop, #reset").prop('disabled', false);
    if(intervalId !== null){return;}
-   let pre=new Date();
-   intervalId=setInterval(function(){
-      const now =new Date();
-      elapsed+=now-pre;
-      pre=now;
+   let pre = new Date();
+   intervalId = setInterval(function(){
+      const now = new Date();
+      elapsed += now - pre;
+      pre = now;
       updateTime();
       },10);
    });
 
 stop.addEventListener('click',function(e){
+   $("#stop").prop('disabled', true);
+   $("#start, #reset").prop('disabled', false);
    clearInterval(intervalId);
-   intervalId=null;
+   intervalId = null;
 });
 
 reset.addEventListener('click',function(e){
-   elapsed=0;
+   $("#reset,#stop").prop('disabled', true);
+   $("#start").prop('disabled', false);
+   elapsed = 0;
    updateTime();
 });
